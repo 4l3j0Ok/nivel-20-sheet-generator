abf2b59e2bbc2dce2ff686b8be67f29e4d8827a5
4l3j0Ok <alejofsarmiento@gmail.com>
2025-03-22 20:29:41 -0300

    feat: add node adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 import icon from 'astro-icon';
-
+import node from '@astrojs/node';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   site: "https://sheet-generator.alejoide.com",
   integrations: [icon(), react()],
   env: {
